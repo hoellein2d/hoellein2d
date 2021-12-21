a6dd0ebdd2daaa5374b26db4f39dcc068d16f0d4
b3nks <github@postelio.de>
2021-12-21 22:20:52 +0100

    Updates

---
+++ bonfire.tsx
@@ -0,0 +1,106 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="bonfire" tilewidth="32" tileheight="32" tilecount="64" columns="16">
+ <image source="../../../../../../Hölle/rc3/AlteMap/rc3world/bonfire.png" width="512" height="128"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="200"/>
+   <frame tileid="2" duration="200"/>
+   <frame tileid="4" duration="200"/>
+   <frame tileid="6" duration="200"/>
+   <frame tileid="8" duration="200"/>
+   <frame tileid="10" duration="200"/>
+   <frame tileid="12" duration="200"/>
+   <frame tileid="14" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="1">
+  <animation>
+   <frame tileid="1" duration="200"/>
+   <frame tileid="3" duration="200"/>
+   <frame tileid="5" duration="200"/>
+   <frame tileid="7" duration="200"/>
+   <frame tileid="9" duration="200"/>
+   <frame tileid="11" duration="200"/>
+   <frame tileid="13" duration="200"/>
+   <frame tileid="15" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="16">
+  <animation>
+   <frame tileid="16" duration="200"/>
+   <frame tileid="18" duration="200"/>
+   <frame tileid="20" duration="200"/>
+   <frame tileid="22" duration="200"/>
+   <frame tileid="24" duration="200"/>
+   <frame tileid="26" duration="200"/>
+   <frame tileid="28" duration="200"/>
+   <frame tileid="30" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="17">
+  <animation>
+   <frame tileid="17" duration="200"/>
+   <frame tileid="19" duration="200"/>
+   <frame tileid="21" duration="200"/>
+   <frame tileid="23" duration="200"/>
+   <frame tileid="25" duration="200"/>
+   <frame tileid="27" duration="200"/>
+   <frame tileid="29" duration="200"/>
+   <frame tileid="31" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="32">
+  <animation>
+   <frame tileid="32" duration="200"/>
+   <frame tileid="34" duration="200"/>
+   <frame tileid="36" duration="200"/>
+   <frame tileid="38" duration="200"/>
+   <frame tileid="40" duration="200"/>
+   <frame tileid="42" duration="200"/>
+   <frame tileid="44" duration="200"/>
+   <frame tileid="46" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="33">
+  <animation>
+   <frame tileid="33" duration="200"/>
+   <frame tileid="35" duration="200"/>
+   <frame tileid="37" duration="200"/>
+   <frame tileid="39" duration="200"/>
+   <frame tileid="41" duration="200"/>
+   <frame tileid="43" duration="200"/>
+   <frame tileid="45" duration="200"/>
+   <frame tileid="47" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="48">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+  <animation>
+   <frame tileid="48" duration="200"/>
+   <frame tileid="50" duration="200"/>
+   <frame tileid="52" duration="200"/>
+   <frame tileid="54" duration="200"/>
+   <frame tileid="56" duration="200"/>
+   <frame tileid="58" duration="200"/>
+   <frame tileid="60" duration="200"/>
+   <frame tileid="62" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="49">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+  <animation>
+   <frame tileid="49" duration="200"/>
+   <frame tileid="51" duration="200"/>
+   <frame tileid="53" duration="200"/>
+   <frame tileid="55" duration="200"/>
+   <frame tileid="57" duration="200"/>
+   <frame tileid="59" duration="200"/>
+   <frame tileid="61" duration="200"/>
+   <frame tileid="63" duration="200"/>
+  </animation>
+ </tile>
+</tileset>
